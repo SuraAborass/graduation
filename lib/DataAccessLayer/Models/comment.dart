020c7fbd6adf9d0ef2182c9f67a0a5bf7d81d72e
ahmadherafi <ahmadherafi@gmail.com>
2024-07-14 17:02:49 +0300

    hot fixes

--- lib/DataAccessLayer/Models/comment.dart
+++ lib/DataAccessLayer/Models/comment.dart
@@ -39,7 +39,7 @@ class CommentQuostion {
       section_id: json["section_id"],
       teacher_id: json["teacher_id"],
       comments: Comment.fromJsonList(json),
-      subject: Subject.fromMap(json["subject"]));
+      subject:json["subject"]==null? Subject.zero(): Subject.fromMap(json["subject"]));
 }
 
 class Comment {
